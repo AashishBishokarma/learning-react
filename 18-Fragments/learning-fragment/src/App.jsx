a590e1c9f41fbff63b15ec003423e201de0e6f4b
AashishBishokarma <aashishbishokarma0@gmail.com>
2024-10-13 17:35:59 +0530

    finished the container

--- 18-Fragments/learning-fragment/src/App.jsx
+++ 18-Fragments/learning-fragment/src/App.jsx
@@ -1,6 +1,7 @@
 import ErrorMessage from "./components/ErrorMessage";
 import FoodItems from "./components/FoodItems";
 import "bootstrap/dist/css/bootstrap.min.css";
+import Container from  "./components/Container"
 import "./App.css"
 
 function App() {
@@ -8,12 +9,11 @@ function App() {
   let foodItems = ["Dal","Green vegetable","roti","c","d","e","f",];
   
   return( 
-  <>
-  <h1 className="food-heading">Healthy Food</h1>
-  
-  <ErrorMessage items={foodItems} ></ErrorMessage>
-  <FoodItems items={foodItems}></FoodItems>
-  </>
+  <Container>
+    <h1 className="food-heading">Healthy Food</h1>
+    <ErrorMessage items={foodItems} ></ErrorMessage>
+    <FoodItems items={foodItems}></FoodItems>
+  </Container>
   )
 }
 
